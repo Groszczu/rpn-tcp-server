d0043f2cc8e1465fbdada4c192835bc6b62e645e
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-11-19 18:27:34 +0100

    Create resources repositories (users, history and reports)

---
+++ RPN_Database/ReportRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using RPN_Database.Model;
+
+namespace RPN_Database
+{
+    public class ReportRepository : ContextBasedRepository
+    {
+        private DbSet<Report> Reports { get => _context.Reports; }
+
+        public IEnumerable<Report> All => Reports.ToList();
+
+        public ReportRepository(RpnContext rpnContext) : base(rpnContext)
+        {
+        }
+
+        public async Task<Report> Add(User user, string message)
+        {
+            var newReport = new Report()
+            {
+                User = user,
+                Message = message
+            };
+
+            Reports.Add(newReport);
+
+            await SaveChanges();
+
+            return newReport;
+        }
+    }
+}
---
+++ RPN_TcpServer/UserRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPN_Database;
+using RPN_Database.Model;
+
+using static BCrypt.Net.BCrypt;
+
+namespace RPN_TcpServer
+{
+    class UserRepository
+    {
+        private readonly RpnContext _context;
+        private DbSet<User> AllUsers { get => _context.Users; }
+
+        public HashSet<User> ConnectedUsers { get; }
+
+        public UserRepository(RpnContext context)
+        {
+            _context = context;
+            ConnectedUsers = new HashSet<User>();
+        }
+
+        public User Login(string username, string password)
+        {
+            var user = AllUsers.FirstOrDefault(u => u.Username == username);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("User with given username doesn't exist");
+            }
+
+            if (!EnhancedVerify(password, user.Password))
+            {
+                throw new InvalidOperationException("Invalid password provided");
+            }
+
+            if (ConnectedUsers.Contains(user))
+            {
+                throw new InvalidOperationException("User already logged in");
+            }
+
+            ConnectedUsers.Add(user);
+
+            return user;
+        }
+
+        public User Register(string username, string password)
+        {
+            var user = AllUsers.FirstOrDefault(u => u.Username == username);
+
+            if(user != null)
+            {
+                throw new InvalidOperationException("Username taken");
+            }
+
+            AllUsers.Add(new User()
+            {
+                Created = DateTime.Now,
+                Username = username,
+                Password = EnhancedHashPassword(password)
+            });
+
+            _context.SaveChanges();
+
+            return Login(username, password);
+        }
+
+        public void Logout (User user)
+        {
+            ConnectedUsers.Remove(user);
+        }
+    }
+}
