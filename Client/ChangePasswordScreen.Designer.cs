1a0d71f8ad61ec9d2c13b489ecb5ef69e0e0eda2
bart-kosmala <bartoshmen@gmail.com>
2020-12-03 19:01:22 +0100

    added screen and error handling

---
+++ Client/ChangePasswordScreen.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.ComponentModel;
+
+namespace Client
+{
+    partial class ChangePasswordScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.oldPasswordLabel = new System.Windows.Forms.Label();
+            this.newPasswordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.oldPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.newPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.usernameLabel.Location = new System.Drawing.Point(30, 39);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(105, 17);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username";
+            // 
+            // oldPasswordLabel
+            // 
+            this.oldPasswordLabel.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.oldPasswordLabel.Location = new System.Drawing.Point(30, 101);
+            this.oldPasswordLabel.Name = "oldPasswordLabel";
+            this.oldPasswordLabel.Size = new System.Drawing.Size(105, 17);
+            this.oldPasswordLabel.TabIndex = 1;
+            this.oldPasswordLabel.Text = "Old password";
+            // 
+            // newPasswordLabel
+            // 
+            this.newPasswordLabel.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.newPasswordLabel.Location = new System.Drawing.Point(30, 165);
+            this.newPasswordLabel.Name = "newPasswordLabel";
+            this.newPasswordLabel.Size = new System.Drawing.Size(105, 17);
+            this.newPasswordLabel.TabIndex = 2;
+            this.newPasswordLabel.Text = "New password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.usernameTextBox.Location = new System.Drawing.Point(29, 60);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(244, 23);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // oldPasswordTextBox
+            // 
+            this.oldPasswordTextBox.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.oldPasswordTextBox.Location = new System.Drawing.Point(30, 121);
+            this.oldPasswordTextBox.Name = "oldPasswordTextBox";
+            this.oldPasswordTextBox.Size = new System.Drawing.Size(244, 23);
+            this.oldPasswordTextBox.TabIndex = 4;
+            this.oldPasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // newPasswordTextBox
+            // 
+            this.newPasswordTextBox.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.newPasswordTextBox.Location = new System.Drawing.Point(29, 185);
+            this.newPasswordTextBox.Name = "newPasswordTextBox";
+            this.newPasswordTextBox.Size = new System.Drawing.Size(244, 23);
+            this.newPasswordTextBox.TabIndex = 5;
+            this.newPasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.submitButton.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.submitButton.Location = new System.Drawing.Point(99, 235);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(98, 29);
+            this.submitButton.TabIndex = 6;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // ChangePasswordScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 297);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.newPasswordTextBox);
+            this.Controls.Add(this.oldPasswordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.newPasswordLabel);
+            this.Controls.Add(this.oldPasswordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ChangePasswordScreen";
+            this.Text = "Change your password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox newPasswordTextBox;
+        private System.Windows.Forms.TextBox oldPasswordTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox usernameTextBox;
+
+        private System.Windows.Forms.Label newPasswordLabel;
+        private System.Windows.Forms.Label oldPasswordLabel;
+
+        private System.Windows.Forms.Label usernameLabel;
+
+        #endregion
+    }
+}
